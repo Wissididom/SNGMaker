0bca2c255ea8fbf3e91acc7ecd88eac8e82fa382
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-09-29 20:34:45 +0200

    Try switching to ESM

--- src/app/user-editor/user-editor.component.ts
+++ src/app/user-editor/user-editor.component.ts
@@ -1,12 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { ReplaySubject, Observable } from 'rxjs';
 
-declare global {
-  interface Window {
-    require: any;
-    monaco: typeof import('monaco-editor');
-  }
-}
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 @Component({
   selector: 'user-editor',
@@ -17,148 +11,128 @@ declare global {
 export class UserEditorComponent implements AfterViewInit {
   @ViewChild('editorContainer', { static: true }) containerRef!: ElementRef<HTMLDivElement>;
   private editor?: import('monaco-editor').editor.IStandaloneCodeEditor;
-  private monaco$ = new ReplaySubject<typeof import('monaco-editor')>(1);
 
   getEditor() {
     return this.editor;
   }
 
   async ngAfterViewInit(): Promise<void> {
-    this.loadMonaco().subscribe(monaco => {
-      monaco.languages.register({ id: 'sngFile'});
-      monaco.languages.setMonarchTokensProvider('sngFile', {
-        tokenizer: {
-          root: [
-            [/^[Uu][Nn][Bb][Ee][Kk][Aa][Nn][Nn][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unbekannt'],
-            [/^[Uu][Nn][Bb][Ee][Nn][Aa][Nn][Nn][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unbenannt'],
-            [/^[Uu][Nn][Kk][Nn][Oo][Ww][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unknown'],
-            [/^[Ii][Nn][Tt][Rr][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'intro'],
-            [/^[Vv][Ee][Rr][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'vers'],
-            [/^[Vv][Ee][Rr][Ss][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'verse'],
-            [/^[Ss][Tt][Rr][Oo][Pp][Hh][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'strophe'],
-            [/^[Pp][Rr][Ee]-[Bb][Rr][Ii][Dd][Gg][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-bridge'],
-            [/^[Bb][Rr][Ii][Dd][Gg][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'bridge'],
-            [/^[Mm][Ii][Ss][Cc](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'misc'],
-            [/^[Pp][Rr][Ee]-[Rr][Ee][Ff][Rr][Aa][Ii][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-refrain'],
-            [/^[Rr][Ee][Ff][Rr][Aa][Ii][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'refrain'],
-            [/^[Pp][Rr][Ee]-[Cc][Hh][Oo][Rr][Uu][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-chorus'],
-            [/^[Cc][Hh][Oo][Rr][Uu][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'chorus'],
-            [/^[Pp][Rr][Ee]-[Cc][Oo][Dd][Aa](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-coda'],
-            [/^[Cc][Oo][Dd][Aa](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'coda'],
-            [/^[Zz][Ww][Ii][Ss][Cc][Hh][Ee][Nn][Ss][Pp][Ii][Ee][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'zwischenspiel'],
-            [/^[Ii][Nn][Tt][Ee][Rr][Ll][Uu][Dd][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'interlude'],
-            [/^[Ii][Nn][Ss][Tt][Rr][Uu][Mm][Ee][Nn][Tt][Aa][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'instrumental'],
-            [/^[Ee][Nn][Dd][Ii][Nn][Gg](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'ending'],
-            [/^[Ee][Nn][Dd][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'ende'],
-            [/^[Oo][Uu][Tt][Rr][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'outro'],
-            [/^[Tt][Ee][Ii][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'teil'],
-            [/^[Pp][Aa][Rr][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'part'],
-            [/^[Cc][Hh][Oo][Rr](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'chor'],
-            [/^[Ss][Oo][Ll][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'solo'],
-            [/^[Bb][Rr][Ee][Aa][Kk][Dd][Oo][Ww][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'breakdown'],
-            [/^[Vv][Aa][Mm][Pp](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'vamp'],
-            [/^[Tt][Uu][Rr][Nn][Aa][Rr][Oo][Uu][Nn][Dd](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'turnaround'],
-            [/^[Tt][Aa][Gg](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'tag'],
-            [/^[Aa][Nn][Dd][Ee][Rr][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'andere'],
-            [/^[Cc][Oo][Pp][Yy][Rr][Ii][Gg][Hh][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'copyright'],
-            [/^---?-?$/m, 'hyphens'],
-          ]
-        }
-      });
-      monaco.editor.defineTheme('sngTheme', {
-        base: 'vs-dark',
-        inherit: true,
-        rules: [
-          { token: 'unbekannt', foreground: '5DBCD2' },
-          { token: 'unbenannt', foreground: '5DBCD2' },
-          { token: 'unknown', foreground: '5DBCD2' },
-          { token: 'vers', foreground: '0080FF' },
-          { token: 'verse', foreground: '0080FF' },
-          { token: 'strophe', foreground: '0080FF' },
-          { token: 'pre-bridge', foreground: 'AA55FF' },
-          { token: 'bridge', foreground: 'CC0000' },
-          { token: 'misc', foreground: 'CC0000' },
-          { token: 'pre-refrain', foreground: 'AA55FF' },
-          { token: 'refrain', foreground: '8000FF' },
-          { token: 'pre-chorus', foreground: 'AA55FF' },
-          { token: 'chorus', foreground: '8000FF' },
-          { token: 'pre-coda', foreground: 'AA55FF' },
-          { token: 'coda', foreground: '008040' },
-          { token: 'zwischenspiel', foreground: 'CC0000' },
-          { token: 'interlude', foreground: 'CC0000' },
-          { token: 'instrumental', foreground: 'CC0000' },
-          { token: 'ending', foreground: '008040' },
-          { token: 'ende', foreground: '008040' },
-          { token: 'outro', foreground: '008040' },
-          { token: 'teil', foreground: '0080FF' },
-          { token: 'part', foreground: '0080FF' },
-          { token: 'chor', foreground: 'AAB0FF' },
-          { token: 'solo', foreground: 'AAB0FF' },
-          { token: 'breakdown', foreground: '008040' },
-          { token: 'vamp', foreground: '008040' },
-          { token: 'turnaround', foreground: 'CC0000' },
-          { token: 'tag', foreground: 'CC0000' },
-          { token: 'andere', foreground: 'CC0000' },
-          { token: 'copyright', foreground: 'FFFF00' },
-          { token: 'hyphens', foreground: 'CC0000' },
-          // TODO: https://github.com/microsoft/monaco-editor/issues/4462
-          /*{ token: 'unbekannt', background: '5DBCD2', foreground: 'FFFFFF' },
-          { token: 'unbenannt', background: '5DBCD2', foreground: 'FFFFFF' },
-          { token: 'unknown', background: '5DBCD2', foreground: 'FFFFFF' },
-          { token: 'vers', background: '0080FF', foreground: 'FFFFFF' },
-          { token: 'verse', background: '0080FF', foreground: 'FFFFFF' },
-          { token: 'strophe', background: '0080FF', foreground: 'FFFFFF' },
-          { token: 'pre-bridge', background: 'AA55FF', foreground: 'FFFFFF' },
-          { token: 'bridge', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'misc', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'pre-refrain', background: 'AA55FF', foreground: 'FFFFFF' },
-          { token: 'refrain', background: '8000FF', foreground: 'FFFFFF' },
-          { token: 'pre-chorus', background: 'AA55FF', foreground: 'FFFFFF' },
-          { token: 'chorus', background: '8000FF', foreground: 'FFFFFF' },
-          { token: 'pre-coda', background: 'AA55FF', foreground: 'FFFFFF' },
-          { token: 'coda', background: '008040', foreground: 'FFFFFF' },
-          { token: 'zwischenspiel', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'interlude', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'instrumental', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'ending', background: '008040', foreground: 'FFFFFF' },
-          { token: 'ende', background: '008040', foreground: 'FFFFFF' },
-          { token: 'outro', background: '008040', foreground: 'FFFFFF' },
-          { token: 'teil', background: '0080FF', foreground: 'FFFFFF' },
-          { token: 'part', background: '0080FF', foreground: 'FFFFFF' },
-          { token: 'chor', background: 'AAB0FF', foreground: 'FFFFFF' },
-          { token: 'solo', background: 'AAB0FF', foreground: 'FFFFFF' },
-          { token: 'breakdown', background: '008040', foreground: 'FFFFFF' },
-          { token: 'vamp', background: '008040', foreground: 'FFFFFF' },
-          { token: 'turnaround', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'tag', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'andere', background: 'CC0000', foreground: 'FFFFFF' },
-          { token: 'copyright', background: 'FFFF00', foreground: 'FFFFFF' },
-          { token: 'hyphens', foreground: 'CC0000' },*/
-        ],
-        colors: {},
-      });
-      this.editor = monaco.editor.create(this.containerRef.nativeElement, {
-        value: '',
-        language: 'sngFile',
-        theme: 'sngTheme'
-      });
+    monaco.languages.register({ id: 'sngFile'});
+    monaco.languages.setMonarchTokensProvider('sngFile', {
+      tokenizer: {
+        root: [
+          [/^[Uu][Nn][Bb][Ee][Kk][Aa][Nn][Nn][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unbekannt'],
+          [/^[Uu][Nn][Bb][Ee][Nn][Aa][Nn][Nn][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unbenannt'],
+          [/^[Uu][Nn][Kk][Nn][Oo][Ww][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'unknown'],
+          [/^[Ii][Nn][Tt][Rr][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'intro'],
+          [/^[Vv][Ee][Rr][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'vers'],
+          [/^[Vv][Ee][Rr][Ss][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'verse'],
+          [/^[Ss][Tt][Rr][Oo][Pp][Hh][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'strophe'],
+          [/^[Pp][Rr][Ee]-[Bb][Rr][Ii][Dd][Gg][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-bridge'],
+          [/^[Bb][Rr][Ii][Dd][Gg][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'bridge'],
+          [/^[Mm][Ii][Ss][Cc](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'misc'],
+          [/^[Pp][Rr][Ee]-[Rr][Ee][Ff][Rr][Aa][Ii][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-refrain'],
+          [/^[Rr][Ee][Ff][Rr][Aa][Ii][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'refrain'],
+          [/^[Pp][Rr][Ee]-[Cc][Hh][Oo][Rr][Uu][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-chorus'],
+          [/^[Cc][Hh][Oo][Rr][Uu][Ss](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'chorus'],
+          [/^[Pp][Rr][Ee]-[Cc][Oo][Dd][Aa](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'pre-coda'],
+          [/^[Cc][Oo][Dd][Aa](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'coda'],
+          [/^[Zz][Ww][Ii][Ss][Cc][Hh][Ee][Nn][Ss][Pp][Ii][Ee][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'zwischenspiel'],
+          [/^[Ii][Nn][Tt][Ee][Rr][Ll][Uu][Dd][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'interlude'],
+          [/^[Ii][Nn][Ss][Tt][Rr][Uu][Mm][Ee][Nn][Tt][Aa][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'instrumental'],
+          [/^[Ee][Nn][Dd][Ii][Nn][Gg](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'ending'],
+          [/^[Ee][Nn][Dd][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'ende'],
+          [/^[Oo][Uu][Tt][Rr][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'outro'],
+          [/^[Tt][Ee][Ii][Ll](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'teil'],
+          [/^[Pp][Aa][Rr][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'part'],
+          [/^[Cc][Hh][Oo][Rr](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'chor'],
+          [/^[Ss][Oo][Ll][Oo](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'solo'],
+          [/^[Bb][Rr][Ee][Aa][Kk][Dd][Oo][Ww][Nn](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'breakdown'],
+          [/^[Vv][Aa][Mm][Pp](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'vamp'],
+          [/^[Tt][Uu][Rr][Nn][Aa][Rr][Oo][Uu][Nn][Dd](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'turnaround'],
+          [/^[Tt][Aa][Gg](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'tag'],
+          [/^[Aa][Nn][Dd][Ee][Rr][Ee](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'andere'],
+          [/^[Cc][Oo][Pp][Yy][Rr][Ii][Gg][Hh][Tt](?:(?: (?:[A-Z0-9][0-9a-z]*))?| +)$/m, 'copyright'],
+          [/^---?-?$/m, 'hyphens'],
+        ]
+      }
     });
-  }
-
-  loadMonaco(): Observable<typeof import('monaco-editor')> {
-    if (window.monaco) {
-      this.monaco$.next(window.monaco);
-      return this.monaco$;
-    }
-    const loaderScript = document.createElement('script');
-    loaderScript.src = '/assets/lib/monaco/vs/loader.js';
-    loaderScript.onload = () => {
-      window.require.config({ paths: { vs: '/assets/lib/monaco/vs' } });
-      window.require(['vs/editor/editor.main'], () => {
-        this.monaco$.next(window.monaco);
-      });
-    };
-    document.body.appendChild(loaderScript);
-    return this.monaco$;
+    monaco.editor.defineTheme('sngTheme', {
+      base: 'vs-dark',
+      inherit: true,
+      rules: [
+        { token: 'unbekannt', foreground: '5DBCD2' },
+        { token: 'unbenannt', foreground: '5DBCD2' },
+        { token: 'unknown', foreground: '5DBCD2' },
+        { token: 'vers', foreground: '0080FF' },
+        { token: 'verse', foreground: '0080FF' },
+        { token: 'strophe', foreground: '0080FF' },
+        { token: 'pre-bridge', foreground: 'AA55FF' },
+        { token: 'bridge', foreground: 'CC0000' },
+        { token: 'misc', foreground: 'CC0000' },
+        { token: 'pre-refrain', foreground: 'AA55FF' },
+        { token: 'refrain', foreground: '8000FF' },
+        { token: 'pre-chorus', foreground: 'AA55FF' },
+        { token: 'chorus', foreground: '8000FF' },
+        { token: 'pre-coda', foreground: 'AA55FF' },
+        { token: 'coda', foreground: '008040' },
+        { token: 'zwischenspiel', foreground: 'CC0000' },
+        { token: 'interlude', foreground: 'CC0000' },
+        { token: 'instrumental', foreground: 'CC0000' },
+        { token: 'ending', foreground: '008040' },
+        { token: 'ende', foreground: '008040' },
+        { token: 'outro', foreground: '008040' },
+        { token: 'teil', foreground: '0080FF' },
+        { token: 'part', foreground: '0080FF' },
+        { token: 'chor', foreground: 'AAB0FF' },
+        { token: 'solo', foreground: 'AAB0FF' },
+        { token: 'breakdown', foreground: '008040' },
+        { token: 'vamp', foreground: '008040' },
+        { token: 'turnaround', foreground: 'CC0000' },
+        { token: 'tag', foreground: 'CC0000' },
+        { token: 'andere', foreground: 'CC0000' },
+        { token: 'copyright', foreground: 'FFFF00' },
+        { token: 'hyphens', foreground: 'CC0000' },
+        // TODO: https://github.com/microsoft/monaco-editor/issues/4462
+        /*{ token: 'unbekannt', background: '5DBCD2', foreground: 'FFFFFF' },
+        { token: 'unbenannt', background: '5DBCD2', foreground: 'FFFFFF' },
+        { token: 'unknown', background: '5DBCD2', foreground: 'FFFFFF' },
+        { token: 'vers', background: '0080FF', foreground: 'FFFFFF' },
+        { token: 'verse', background: '0080FF', foreground: 'FFFFFF' },
+        { token: 'strophe', background: '0080FF', foreground: 'FFFFFF' },
+        { token: 'pre-bridge', background: 'AA55FF', foreground: 'FFFFFF' },
+        { token: 'bridge', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'misc', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'pre-refrain', background: 'AA55FF', foreground: 'FFFFFF' },
+        { token: 'refrain', background: '8000FF', foreground: 'FFFFFF' },
+        { token: 'pre-chorus', background: 'AA55FF', foreground: 'FFFFFF' },
+        { token: 'chorus', background: '8000FF', foreground: 'FFFFFF' },
+        { token: 'pre-coda', background: 'AA55FF', foreground: 'FFFFFF' },
+        { token: 'coda', background: '008040', foreground: 'FFFFFF' },
+        { token: 'zwischenspiel', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'interlude', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'instrumental', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'ending', background: '008040', foreground: 'FFFFFF' },
+        { token: 'ende', background: '008040', foreground: 'FFFFFF' },
+        { token: 'outro', background: '008040', foreground: 'FFFFFF' },
+        { token: 'teil', background: '0080FF', foreground: 'FFFFFF' },
+        { token: 'part', background: '0080FF', foreground: 'FFFFFF' },
+        { token: 'chor', background: 'AAB0FF', foreground: 'FFFFFF' },
+        { token: 'solo', background: 'AAB0FF', foreground: 'FFFFFF' },
+        { token: 'breakdown', background: '008040', foreground: 'FFFFFF' },
+        { token: 'vamp', background: '008040', foreground: 'FFFFFF' },
+        { token: 'turnaround', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'tag', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'andere', background: 'CC0000', foreground: 'FFFFFF' },
+        { token: 'copyright', background: 'FFFF00', foreground: 'FFFFFF' },
+        { token: 'hyphens', foreground: 'CC0000' },*/
+      ],
+      colors: {},
+    });
+    this.editor = monaco.editor.create(this.containerRef.nativeElement, {
+      value: '',
+      language: 'sngFile',
+      theme: 'sngTheme'
+    });
   }
 }
